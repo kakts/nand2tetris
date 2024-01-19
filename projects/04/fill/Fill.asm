028545abe25d0c4b3148c52bee2a247c3c391fda
kakts <kakts.git@gmail.com>
2024-01-20 01:04:52 +0900

    feat: 4.4 Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,47 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+(INIT)
+
+    @8192 // 32 * 256
+    D=A 
+    @R2
+    M=D
+
+(LOOP)
+    @R2
+    M=M-1 // indexを逆順に進行
+    D=M
+    
+    @INIT
+    D;JLT
+    // キーボード入力を監視
+    @KBD
+    D=M
+
+    @WHITEIMP
+    D;JEQ
+
+    // Dの値をチェック 0でなければキーが押されている
+    @BLACKIMP       
+    0;JMP
+
+(WHITEIMP)
+    @SCREEN
+    D=A
+    @R2
+    A=D+M
+    M=0 // ワードを0にすることで16bit分0になる
+    @LOOP
+    0;JMP
+
+// 成功版
+(BLACKIMP)
+    @SCREEN
+    D=A
+    @R2
+    A=D+M
+    M=-1 // 現在のアドレスから1引くことで、1つ前のワード(16bit)全体をまとめて1にすることができる
+    @LOOP
+    0;JMP
+
--- projects/04/mult/Mult_2.asm
+++ projects/04/mult/Mult_2.asm
@@ -8,11 +8,11 @@
     @END
     D;JLE // Dをチェックし0以下になったら@END
     @R0
-    D=M // D = R0
+    D=M // D = R0 // R2にR0を足すためにDに格納
     @R2
-    D=D+M // D +R0
+    D=D+M // D +R0 R2にR0を足す
     @R1
-    M=M-1 // i = i - 1
+    M=M-1 // i = i - 1 ループのカウンタを1減らす
     @LOOP
     0;JMP // LOOPへ移動
 
